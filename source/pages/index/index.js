6c725010d0309ee3ba21e681dc3b555f0d83692d
OrionPro <orion79@rambler.ru>
2019-04-16 00:58:32 +0300

    0 58 16.04.19 убрал лишнее

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,8 +1,8 @@
 import '../../js/common';
 
 require("../../libs/owl.carousel.min");
-import '../../pages/index/index.pug'; //это для обновления страницы при hotreload - при npm build убрать
-import '../../pages/modal.pug'; //это для обновления страницы при hotreload - при npm build убрать
+//import '../../pages/index/index.pug'; //это для обновления страницы при hotreload - при npm build убрать
+//import '../../pages/modal.pug'; //это для обновления страницы при hotreload - при npm build убрать
 import './index.sass';
 
 import '../../js/animate';
